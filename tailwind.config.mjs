3438117ac53fcc7af4112006994cce9d6b3e392d
Jordan Zappitello <j.zappitello57@gmail.com>
2024-10-15 19:27:16 -0600

    added tailwind and a base layout

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      md: "1.25rem",
+      lg: "1.5rem",
+      xl: "1.75rem",
+      "2xl": "2rem",
+      "3xl": "2.25rem",
+      "4xl": "2.5rem",
+      "5xl": "2.75rem",
+      "6xl": "3.25rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
